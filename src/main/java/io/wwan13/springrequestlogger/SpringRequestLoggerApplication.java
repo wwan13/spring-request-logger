465ec4850d9f266fb6fa2911455e3f44b7f6e9a5
wwan13 <wwan13@naver.com>
2024-08-04 15:04:06 +0900

    init : init spring boot project

---
+++ src/main/java/io/wwan13/springrequestlogger/SpringRequestLoggerApplication.java
@@ -0,0 +1,13 @@
+package io.wwan13.springrequestlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRequestLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRequestLoggerApplication.class, args);
+    }
+
+}
