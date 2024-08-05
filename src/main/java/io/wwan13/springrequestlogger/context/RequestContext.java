22678645804b20a38e7aa37c97794aa4e2cbbbeb
wwan13 <wwan13@naver.com>
2024-08-05 13:30:14 +0900

    feat : All Contexts in Request

---
+++ src/main/java/io/wwan13/springrequestlogger/context/RequestContext.java
@@ -0,0 +1,34 @@
+package io.wwan13.springrequestlogger.context;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+record RequestContext(
+        String method,
+        String uri,
+        HttpStatus status,
+        double elapsed,
+        KeyValueContext requestHeaders,
+        KeyValueContext requestParams,
+        JsonContext requestBody,
+        JsonContext responseBody
+) {
+
+    public static RequestContext of(
+            ContentCachingRequestWrapper request,
+            ContentCachingResponseWrapper response,
+            double elapsed
+    ) {
+        return new RequestContext(
+                request.getMethod(),
+                request.getRequestURI(),
+                HttpStatus.valueOf(response.getStatus()),
+                elapsed,
+                KeyValueContext.of(request.getHeaderNames(), request::getHeader),
+                KeyValueContext.of(request.getParameterNames(), request::getParameter),
+                JsonContext.of(request.getContentAsByteArray()),
+                JsonContext.of(response.getContentAsByteArray())
+        );
+    }
+}
